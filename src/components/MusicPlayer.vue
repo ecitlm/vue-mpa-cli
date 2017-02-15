30a50358ba1805cfa3e75c74b3fba7371d1cd57c
ecitlm <ecitlm@163com>
2017-02-16 00:28:57 +0800

    添加audio music播放器

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div id="audio-play">
+    {{songs[0].author}}
     <a-player :music="songs" ref="player"></a-player>
   </div>
 </template>
@@ -7,8 +9,12 @@
 #audio-play{
   position: fixed;
   width:100%;
-  bottom:0;
+  bottom:5px;
   left:0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  box-shadow: 3px 3px 8px #ddd;
 }
 #audio-play .aplayer{
     margin:0;}
@@ -27,7 +33,7 @@
         songs: [
           {
             title: '你好世界2',
-            author: '我不好 ',
+            author: ' 王力宏-',
             url: 'http://m2.music.126.net/9SmIPoDjeEJCQZtaoFkVaA==/1076421883600633.mp3',
             pic: 'http://p4.music.126.net/qjgZCvf7hf-8St7LuZrcOg==/1362294906860186.jpg',
             lrc: '[00:00.00]lrc here\n[00:01.00]aplayer'
@@ -39,8 +45,9 @@
       // 深度 watcher
       songs: {
         handler: function (val, oldVal) {
-          let aplayer = this.$refs.player.control
-          aplayer.play();
+          let aplayer = this.$refs.player.control;
+          aplayer.pause();
+          console.log(aplayer.play())
         },
         deep: true
       }
@@ -54,7 +61,8 @@
           console.log(res.data);
           this.list = res.data.songs[0];
           this.songs[0].title=this.list.name;
-          this.songs[0].author=this.list.mp3Url;
+          this.songs[0].author=this.list.name;
+          this.songs[0].url=this.list.mp3Url
           this.songs[0].pic=this.list.album.picUrl;
 
 
