009e0935f78133dfc40c5b6d0c83f47e82c58b08
ecitlm <ecitlm@163.com>
2018-07-05 21:25:39 +0800

    feat:init vue-mpa-cli

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var express = require('express')
-var app = express()
-
+const express = require('express')
+const app = express()
 app.use(express.static('./dist'))
-
 app.get('/', function (req, res) {
-  res.send('Hello Vue')
+  res.send('Hello Vue-mpa-cli')
 })
 
 app.listen(2333)
-console.log('server start 2333')
+console.log('server start http://120.0.0.1:2333')
